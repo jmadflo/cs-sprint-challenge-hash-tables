0aa3793d7cad7e7ef96a63248fe395a956d96bd2
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-07-10 09:22:36 -0700

    3 works, but can't solve large test efficiently

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,8 +3,16 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
-
-    return result
+    round_survivors = {0:arrays[0]}
+    current = 1
+    while current < len(arrays):
+        print(current)
+        round_survivors[current] = []
+        for num in arrays[current]:
+            if num in round_survivors[current - 1]:
+                round_survivors[current].append(num)
+        current += 1
+    return round_survivors[current - 1]
 
 
 if __name__ == "__main__":

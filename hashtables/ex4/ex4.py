68773f8bda2346454c31d3ea80f44902d054e310
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-07-10 09:58:21 -0700

    ex4 works

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,7 +3,11 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
-
+    nums = dict(zip(a, [True] * len(a)))
+    result = []
+    for num in a:
+        if -num in nums and num > 0:
+            result.append(num)
     return result
 
 

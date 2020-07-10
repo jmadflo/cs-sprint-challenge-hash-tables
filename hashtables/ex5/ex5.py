a5b6e5f28bf4c6211b58e9002be144a557eabb19
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-07-10 10:29:15 -0700

    ex5 passes all tests efficiently

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -7,6 +7,17 @@ def finder(files, queries):
     YOUR CODE HERE
     """
     # Your code here
+    queries_set = dict(zip(queries, [True] * len(queries)))
+    result = []
+
+    for file in files:
+        current = -1
+        while True:
+            if file[current] == '/':
+                if file[current + 1:] in queries_set:
+                    result.append(file)
+                break
+            current -= 1
 
     return result
 
